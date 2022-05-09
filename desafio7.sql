8d051516a4059b918a49785212f882016576be4d
Victor Hugo <victorhsmsantos@gmail.com>
2022-05-09 14:45:07 -0300

    Requisito 7 feito

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT
+  artists.artist_name AS artista,
+  albums.album_title AS album,
+  COUNT(artists_followed.artist_id) AS seguidores
+FROM
+  SpotifyClone.artists AS artists
+  INNER JOIN SpotifyClone.albums AS albums ON albums.artist_id = artists.artist_id
+  INNER JOIN SpotifyClone.artists_followed AS artists_followed ON artists_followed.artist_id = artists.artist_id
+GROUP BY albums.album_id
+ORDER BY
+  seguidores DESC,
+  artists.artist_name,
+  albums.album_title;
