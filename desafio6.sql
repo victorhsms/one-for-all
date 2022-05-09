1e6b92ad9264f498e23e8f359bcaafa1399c827c
Victor Hugo <victorhsmsantos@gmail.com>
2022-05-09 14:26:45 -0300

    Colocado ROUND no faturamento total pra estilização da exibição do resultado

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ SELECT
   MIN(plans.plan_value) AS faturamento_minimo,
   MAX(plans.plan_value) AS faturamento_maximo,
   ROUND(AVG(plans.plan_value), 2) AS faturamento_medio,
-  SUM(plans.plan_value) AS faturamento_total
+  ROUND(SUM(plans.plan_value), 2) AS faturamento_total
 FROM SpotifyClone.plans AS plans
 INNER JOIN SpotifyClone.users AS users
 ON users.plan_id = plans.plan_id;
